d7d136e16da4afcfbf322d725ad03398764bfbef
Dylan Allbee <dylan@allbee.org>
2019-06-02 17:25:43 -0700

    Stub out config

--- raw.go
+++ raw.go
@@ -11,8 +11,36 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// from: http://man7.org/linux/man-pages/man8/mount.fuse.8.html
+// we may want to not support all of these. Just listing them for now.
+type Options struct {
+	// our options
+	Debug bool
+
+	// mount options
+	DefaultPermissions bool
+	AllowOther bool
+	RootMode uint32
+	BlockDevice bool
+	BlockSize int
+	MaxRead int
+	FD int
+	UID int
+	GID int
+	FSName string
+	SubType string
+
+	// libfuse options
+	AllowRoot bool
+	AutoUnmount bool // can we make this default behavior? It's convenient.
+}
+
+func (o Options) String() string {
+	return ""
+}
+
 // WIP
-func mount(target string, opts string) (err error) {
+func mount(target string, options Options) (err error) {
 	// create the mount directory
 	if _, err := os.Stat(target); os.IsNotExist(err) {
 		if err := os.Mkdir(target, 0777); err != nil {
@@ -37,9 +65,8 @@ func mount(target string, opts string) (err error) {
 	}()
 
 	// register the mount
-	// todo: args
 	var stderr bytes.Buffer
-	cmd := exec.Command("fusermount", target)
+	cmd := exec.Command("fusermount", target, options.String())
 	cmd.Stderr = &stderr
 	cmd.Env = []string{"_FUSE_COMMFD=3"}
 	cmd.ExtraFiles = []*os.File{fd}
